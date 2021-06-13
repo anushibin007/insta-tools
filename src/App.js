9512514098a26131cc7d6da9d03eaec3874683f6
Anu Shibin <anushibin007@gmail.com>
2021-06-13 15:05:54 +0530

    Initial app framework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "animate.css/animate.min.css";
+
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Navigation />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import "../css/navbar.css";
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import About from "./About";
+
+function Navigation() {
+	return (
+		<div>
+			<Navbar bg="light" expand="lg">
+				<Navbar.Brand>Instagram Tools</Navbar.Brand>
+				<Navbar.Toggle aria-controls="insta-tools-navbar" />
+				<Navbar.Collapse id="insta-tools-navbar">
+					<Nav className="mr-auto">
+						<About />
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			{/* This line is to make an AJAX call to the counter update function. This will just increment the counter in the background */}
+			<img src="https://www.freevisitorcounters.com/en/home/counter/836517/t/0" style={{ display: "none" }}></img>
+		</div>
+	);
+}
+
+export default Navigation;
